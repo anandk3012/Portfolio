cd6c8cb0df9f05ce68af9c1986311443cd59778c
anandk3012 <anandkash99@gmail.com>
2024-02-20 20:32:23 +0530

    minor changes

--- src/assets/components/about-page/Landing.jsx
+++ src/assets/components/about-page/Landing.jsx
@@ -3,10 +3,10 @@ import me2 from '../../images/me2.png'
 
 const Landing = () => {
   return (
-    <div className='w-10/12 h-auto mx-auto mt-8 flex border-blue-900 rounded-xl bg-white'>
+    <div className='w-10/12 h-auto mx-auto mt-8 flex border-blue-900 rounded-xl bg-white scroll-smooth'>
         <div className="text w-2/3  p-6">
             <div className="heading p-3">
-                <p className='text-4xl text-cyan-950 font-bold'>Hello! I'm Anand, a part-time Front-end Developer and full-time Student ;)</p>
+                <p className='text-4xl text-cyan-950 font-bold'>Hello! I'm Anand, a part-time React Front-end Developer and full-time Student ;)</p>
             </div>
 
             <div className="content p-3 text-lg font-normal text-black text-wrap">
@@ -14,7 +14,7 @@ const Landing = () => {
                 <p className='p-2'>I am a student at the Indian Institute of Technology, Hyderabad (fondly known as IITH) majoring in Physics ⚛.I also worked on various freelancing projects for startups and was the Web Development team Coordinator for the Finance and Consulting Club💲 (FCC) at IITH for the year 2023-24.
                 </p>
                 <p className='p-2'>Through these experiences I learned a lot about Team-work, Project Management, Networking and developed a working style that leans towards flexibility, clarity and collaboration</p>
-                <p>I'm currently looking for an Software Developer Internship. If you're willing to give me an opportunity, do check out my Github and Linkedin pages below ⬇</p>
+                <p className='p-2'>I'm currently looking for an Software Developer Internship. If you're willing to give me an opportunity, do check out my Github and Linkedin pages below ⬇</p>
             </div>
 
             <div className="buttons w-full mt-4 p-3 flex">
