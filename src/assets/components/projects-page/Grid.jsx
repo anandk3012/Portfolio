200a11ce20a1447dcd191083b5012e6a5c0e2079
Anand <126659841+anandk3012@users.noreply.github.com>
2024-06-25 17:19:54 +0530

    Update Grid.jsx

--- src/assets/components/projects-page/Grid.jsx
+++ src/assets/components/projects-page/Grid.jsx
@@ -12,6 +12,12 @@ const Grid = () => {
             <a href="https://fcc-did.pages.dev/" className='w-1/3 h-auto' target='_blank'>
                 <Card img={fcc} title="FCC IITH" text="A static website that I made for FCC IITH using HTML,CSS and JS primarily for web layout, Bootstrap Framework for styling and GSAP scroll animations" />
             </a>
+            <a href="https://tickytack.netlify.app/" className='w-1/3 h-auto' target='_blank'>
+                <Card img={fcc} title="Tic tac toe" text="Tic tac toe game using Reactjs, TailwindCSS" />
+            </a>
+            <a href="https://github.com/anandk3012/Spin-lattice-metropolis/" className='w-1/3 h-auto' target='_blank'>
+                <Card img={fcc} title="Metropolis algo" text="Implemented the Metropolis algorithm to calculate the magnetization a spin-lattice of NxN atoms using Python and MatplotLib" />
+            </a>
         </div>)
 }
 
