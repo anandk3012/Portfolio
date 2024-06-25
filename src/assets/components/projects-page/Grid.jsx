87609d0fba415f73548ecad15ff3d8233738b1d7
Anand <126659841+anandk3012@users.noreply.github.com>
2024-06-25 17:21:37 +0530

    Update Grid.jsx

--- src/assets/components/projects-page/Grid.jsx
+++ src/assets/components/projects-page/Grid.jsx
@@ -5,7 +5,7 @@ import fcc from  '../../images/fcc.jpg'
 
 const Grid = () => {
     return (
-        <div className='h-auto w-5/6 mx-auto flex flex-wrap justify-evenly'>
+        <div className='h-auto w-5/6 mx-auto flex flex-wrap gap-5 justify-evenly'>
             <a href="https://anandk3012.github.io/DeskPe/" className='w-1/3 h-auto' target='_blank'>
                 <Card img={deskpe} title="Desk Pe Website" text="A static website that I made for a start up fair using HTML,CSS and JS primarily for web layout and GSAP scroll animations " />
             </a>
